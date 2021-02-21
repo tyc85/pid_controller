9a119f31e86d75504ceef25a4aa7eabfd9efd4e7
Tsung-Yi Chen <chent6@corning.com>
2021-02-20 23:51:01 -0800

    script to show tcl version

---
+++ examples/gui/show_version.py
@@ -0,0 +1,6 @@
+import PySimpleGUI as sg
+
+print(sg)             # Location of your PySimpleGUI.py file
+print(sg.version)     # PySimpleGUI version number
+print(sg.tclversion_detailed)   # tkinter detailed version number (in PySimpleGUI version 4.29.0+)
+print(sg.sys.version) # Python version number
